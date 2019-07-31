67fbd1bf2017644fa1a330444097e5daf85fecc7
metiro14 <51830619+metiro14@users.noreply.github.com>
2019-08-01 02:50:04 +0300

    TransferPage
    Need BackEnd

---
+++ lib/TransferFunds.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(new Transfer());
+
+class Transfer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Add balance demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new TransferPage(title: 'Add Balance'),
+    );
+  }
+}
+class TransferPage extends StatefulWidget {
+  TransferPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _TransferPageState createState() => new _TransferPageState();
+}
+
+
+class _TransferPageState extends State<TransferPage> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new SafeArea(
+          top: false,
+          bottom: false,
+          child: new Form(
+              key: _formKey,
+              autovalidate: true,
+              child: new ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                children: <Widget>[
+                  new TextFormField(
+                    decoration: InputDecoration(
+
+                      hintText: 'Transfer Amount',
+                      labelText: 'Amount',
+                    )
+                    ,
+                    keyboardType: TextInputType.number ,
+                  )
+                  ,
+
+
+                  new TextFormField(
+                    decoration: const InputDecoration(
+
+                      hintText: 'User Email',
+                      labelText: 'Email',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  new Container(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: new RaisedButton(
+                        child: const Text('Submit'),
+                        onPressed: null,        // API HERE ????
+                      )),
+                ],
+              ))),
+    );
+  }
+}
