1aea217f3bf31f2b309c21c99844d981d1657f15
metiro14 <51830619+metiro14@users.noreply.github.com>
2019-07-31 20:04:42 +0300

    AddFunds.dart
    Working on TransferFunds Should be pretty easy :) :) :)

---
+++ lib/AddFunds.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
+
+void main() => runApp(new Funds());
+
+class Funds extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Add balance demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new AddFundsPage(title: 'Add Balance'),
+    );
+  }
+}
+class AddFundsPage extends StatefulWidget {
+  AddFundsPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _AddFundsPageState createState() => new _AddFundsPageState();
+}
+
+class _AddFundsPageState extends State<AddFundsPage> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  var txt = TextEditingController();
+  DateTime date = new DateTime.now();
+
+  Future<Null> selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate:date,
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2022),
+        builder: (BuildContext context, Widget child) {
+          return FittedBox(
+              child: Theme(
+                  child: child,
+                  data: ThemeData(
+                    primaryColor: Colors.blue[300],
+
+                  )));
+        });
+    setState((){
+      date = picked;
+      txt.text = date.toString();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new SafeArea(
+          top: false,
+          bottom: false,
+          child: new Form(
+              key: _formKey,
+              autovalidate: true,
+              child: new ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                children: <Widget>[
+                  new TextFormField(
+                    decoration: const InputDecoration(
+
+                      hintText: 'Enter your first name',
+                      labelText: 'First Name',
+                    ),
+                  ),
+                  new TextFormField(
+                    decoration: const InputDecoration(
+
+                      hintText: 'Enter your last name',
+                      labelText: 'Last Name',
+                    ),
+                  ),
+                  new TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Card Number',
+                      labelText: 'Card Number',
+                    ),
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(15)
+                      , WhitelistingTextInputFormatter.digitsOnly,
+                    ],
+                    keyboardType: TextInputType.number,
+                  ),
+
+                  TextFormField(
+                    controller: txt,
+                    decoration: InputDecoration(
+                        hintText:'Card Expiry Date',
+                        labelText:'Card Expiry Date',
+
+                        suffixIcon: IconButton(
+                            icon: Icon(Icons.calendar_today),
+                            onPressed: () {
+                              selectDate(context);
+                            }
+
+                        )),
+                  ),
+
+                  new TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter CVV number',
+                      labelText: 'CVV',
+
+                    ) ,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(3),
+                      WhitelistingTextInputFormatter.digitsOnly,
+                    ],
+                  )
+                  ,
+                  new Container(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: new RaisedButton(
+                        child: const Text('Submit'),
+                        onPressed: null,
+                        // API ????
+                      )),
+                ],
+              ))),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -21,90 +21,90 @@ class QuickBee extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Stack(
-              alignment: Alignment.topCenter,
-              children: <Widget>[
-                new Image.asset(
-                  'Images/mainLogo.png',
-                  height: 300.0,
-                  width: 300.0,
+  class MyHomePage extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Stack(
+                alignment: Alignment.topCenter,
+                children: <Widget>[
+                  new Image.asset(
+                    'Images/mainLogo.png',
+                    height: 300.0,
+                    width: 300.0,
 
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
 
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => LoginPage(),
-                        ));
-                      },
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ));
+                        },
+                        child: new Container(
+                            alignment: Alignment.center,
+                            height: 60.0,
+                            decoration: new BoxDecoration(
+                                color: Color(0xFF18D191),
+                                borderRadius: new BorderRadius.circular(9.0)),
+                            child: new Text("Sign In With Email",
+                                style: new TextStyle(
+                                    fontSize: 20.0, color: Colors.white))),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20.0, right: 5.0, top: 10.0),
                       child: new Container(
                           alignment: Alignment.center,
                           height: 60.0,
                           decoration: new BoxDecoration(
-                              color: Color(0xFF18D191),
+                              color: Color(0xFF4364A1),
                               borderRadius: new BorderRadius.circular(9.0)),
-                          child: new Text("Sign In With Email",
+                          child: new Text("FaceBook",
                               style: new TextStyle(
                                   fontSize: 20.0, color: Colors.white))),
                     ),
                   ),
-                )
-              ],
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20.0, right: 5.0, top: 10.0),
-                    child: new Container(
-                        alignment: Alignment.center,
-                        height: 60.0,
-                        decoration: new BoxDecoration(
-                            color: Color(0xFF4364A1),
-                            borderRadius: new BorderRadius.circular(9.0)),
-                        child: new Text("FaceBook",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white))),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 10.0, right: 20.0, top: 10.0),
-                    child: new Container(
-                        alignment: Alignment.center,
-                        height: 60.0,
-                        decoration: new BoxDecoration(
-                            color: Color(0xFFDF513B),
-                            borderRadius: new BorderRadius.circular(9.0)),
-                        child: new Text("Google",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white))),
-                  ),
-                )
-              ],
-            )
-          ],
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10.0, right: 20.0, top: 10.0),
+                      child: new Container(
+                          alignment: Alignment.center,
+                          height: 60.0,
+                          decoration: new BoxDecoration(
+                              color: Color(0xFFDF513B),
+                              borderRadius: new BorderRadius.circular(9.0)),
+                          child: new Text("Google",
+                              style: new TextStyle(
+                                  fontSize: 20.0, color: Colors.white))),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
