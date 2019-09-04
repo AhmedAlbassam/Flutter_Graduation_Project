fdc85e04c52437dc8f32eac3c1b94e8b204b94c7
Triplem1997 <51830601+Triplem1997@users.noreply.github.com>
2019-09-04 22:41:15 +0300

    we are making progress

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 import'Event.dart';
 import 'Wallet.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => new _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  // the scroll will be added in this class, and taken data from firebase that addded by org.
+
   int _bottomNavIndex=0;
   @override
+      Firestore _firestore = Firestore.instance;
+       List<DocumentSnapshot> _events = [];
+       bool _loadEvent = true;
+
+    _getEvent() async{
+    Query q = _firestore.collection('Events').orderBy("eventType").limit(5);
+    setState(() {
+       _loadEvent =true;
+    });
+
+        QuerySnapshot _querysnap = await q.getDocuments();
+     _events = _querysnap.documents;
+     
+     setState(() {
+        _loadEvent =false;
+     });
+   
+
+  }
+  void initState(){
+      super.initState();
+      _getEvent();
+  }
   Widget build(BuildContext context) {
     return new Scaffold(
       endDrawer: Drawer(
@@ -56,7 +82,17 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.blueGrey,
           elevation: 0.0,
           iconTheme: new IconThemeData(color: Color(0xFF18D191))),
-      //   body: MainContent(),
+        body: Container(
+          child : _events.length == 0 ? Center(
+            child: Text("there are no events available"),
+          ) : ListView.builder(itemCount: _events.length,itemBuilder: (BuildContext ctx, int i){
+                    
+               return ListTile (
+                 title:Text( _events[i].data['eventType']
+                 ),
+               );
+          }),
+        ),
 
     );
   }
