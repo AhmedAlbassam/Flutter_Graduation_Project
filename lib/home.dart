c70899e283a5404bd9d5164dd9aefe599a0e40f5
Triplem1997 <51830601+Triplem1997@users.noreply.github.com>
2019-09-22 05:54:15 +0300

    we are making progress

--- lib/home.dart
+++ lib/home.dart
@@ -15,52 +15,26 @@ class _HomePageState  extends State<HomePage> {
   @override
       Firestore _firestore = Firestore.instance;
        List<DocumentSnapshot> _events = [];
-       bool _loadEvent = true;
+       bool loadEvent = true;
         ScrollController _scroll = ScrollController();
-
         DocumentSnapshot _lastdoc;
         bool _gettingmoreEvents = false;
         bool _moreEventavaliable = true;
 
     _getEvent() async{
       print('Events are coming');
-    Query q = _firestore.collection('Events').orderBy("eventName").limit(_events.length);
+    Query q = _firestore.collection('Events').orderBy("eventName");
     setState(() {
-       _loadEvent =true;
+       loadEvent =true;
     });
     QuerySnapshot querysnap = await q.getDocuments();
      _events = querysnap.documents;
      _lastdoc = querysnap.documents[querysnap.documents.length-1];
      setState(() {
-        _loadEvent =false;
+        loadEvent =false;
      });
   }
-  _getmoreEvent() async{
-      print('More events coming');
-      _gettingmoreEvents = true;
 
-      if(_moreEventavaliable == false){
-        return;
-      }
-
-      if(_gettingmoreEvents == true){
-        return;
-      }
-    Query q = _firestore.collection('Events')
-        .orderBy("eventName")
-        .startAfter([_lastdoc.data["eventName"]])
-        .limit(_events.length);
-    QuerySnapshot querysnap = await q.getDocuments();
-    if (querysnap.documents.length == 0) {
-      _moreEventavaliable = false;
-    }
-    _lastdoc = querysnap.documents[querysnap.documents.length-1];
-    _events.addAll( querysnap.documents);
-
-    setState(() {
-    });
-      _gettingmoreEvents = false;
-  }
   void initState(){
       super.initState();
       _getEvent();
@@ -70,11 +44,45 @@ class _HomePageState  extends State<HomePage> {
         double currentScroll = _scroll.position.pixels;
         double delta = MediaQuery.of(context).size.height * 0.25;
         if(maxScroll-currentScroll <= delta) {
-          _getmoreEvent();
+          _getEvent();
         }
 
       });
   }
+  Widget displayEvent(){
+      return Container(
+        child : _events.length == 0 ? Center(
+          child: Text("there are no events available"),
+        ) : ListView.builder(
+           controller: _scroll,
+            itemCount: _events.length,
+            itemBuilder: (BuildContext ctx, int i){
+
+              return ListTile (
+
+                title:Text( _events[i].data['eventName']),
+
+                trailing : _events[i].data['Number of tickets'] == 0
+                    ||  _events[i].data['Number of tickets']
+                    == null
+                    ? Icon(Icons.cancel, color: Colors.red,) : Icon(Icons.done, color: Colors.green,),
+
+                onTap:(){
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => Event(_events[i].data['eventName'],
+                        _events[i].data['eventLocation'],
+                        _events[i].data['eventType'],
+                        _events[i].data['eventDate'],
+                        _events[i].data['Number of tickets']),
+
+                  ));
+                },
+
+              );
+            }),
+      );
+  }
+
 
 
 
@@ -124,35 +132,7 @@ class _HomePageState  extends State<HomePage> {
           backgroundColor: Colors.blueGrey,
           elevation: 0.0,
           iconTheme: new IconThemeData(color: Color(0xFF18D191))),
-        body:Container(
-          child : _events.length == 0 ? Center(
-            child: Text("there are no events available"),
-          ) : ListView.builder(
-                controller: _scroll,
-              itemCount: _events.length,
-              itemBuilder: (BuildContext ctx, int i){
-
-            return ListTile (
-
-              title:Text( _events[i].data['eventName']),
-
-              trailing : _events[i].data['Number of tickets'] == 0 ||  _events[i].data['Number of tickets'] == null
-                  ? Icon(Icons.cancel, color: Colors.red,) : Icon(Icons.done, color: Colors.green,),
-
-              onTap:(){
-                Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => Event(_events[i].data['eventName'],
-                        _events[i].data['eventLocation'],
-                        _events[i].data['eventType'],
-                        _events[i].data['eventDate'],
-                        _events[i].data['Number of tickets']),
-
-                ));
-              },
-
-            );
-          }),
-        ),
+        body: displayEvent(),
 
     );
   }
